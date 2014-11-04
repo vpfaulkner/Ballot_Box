17264ef91cbb71e1baf334a63de924ddbb17e34d
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-11-04 14:17:26 -0500

    Added voter create action

--- app/controllers/api/v1/voters_controller.rb
+++ app/controllers/api/v1/voters_controller.rb
@@ -1,2 +1,28 @@
 class API::V1::VotersController < ActionController::API
+  before_filter :load_voter, only: [:show]
+
+
+  def create
+    @voter = Voter.new(voter_params)
+    if @voter.save
+      render json: @voter.as_json(include_token: true), status: :created, location: api_v1_voter_path(@voter)
+    else
+      render json: {errors: @voter.errors}, status: :bad_request
+    end
+  end
+
+  def show
+    render json: @voter
+  end
+
+  private
+
+  def load_voter
+    @voter = Voter.find(params[:id])
+  end
+
+  def voter_params
+    params.require(:voter).permit(:name, :party)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :canidates
+      resources :voters
+      resources :votes
     end
   end
 
